d87b88b67cb6b73f7d62c7896b1380e8155954f2
arthur <arthur.broudoux@gmail.com>
2024-11-17 19:08:52 +0100

    feat: open repository in browser from one command Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- repo.sh
+++ repo.sh
@@ -1,3 +1,22 @@
 function repo() {
-    echo "hello word"
+    repo_ssh_url=$(git config --get remote.origin.url)
+    username=$(echo $repo_ssh_url | awk -F 'git@github.com:|/' '{print $2}')
+    repo_name=$(echo $repo_ssh_url | awk -F '${username}|/' '{print $2}')
+    repo_url="https://github.com/$username/$repo_name"
+    os_type=$(uname)
+
+    case "$os_type" in
+        "Linux")
+            xdg-open "$repo_url" 2>/dev/null || echo "Error: Cannot open the link."
+            ;;
+        "Darwin")
+            open "$repo_url" || echo "Error: Cannot open the link."
+            ;;
+        "CYGWIN"*|"MINGW"*)
+            start "$repo_url" || echo "Error: Cannot open the link."
+            ;;
+        *)
+            echo "Non-supported OS : $os_type"
+            ;;
+    esac
 }
