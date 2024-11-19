71161174fd26406249f4ed7ce96c8549a9fa3392
arthur <arthur.broudoux@gmail.com>
2024-11-19 23:12:38 +0100

    feat: search user command Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- repo.sh
+++ repo.sh
@@ -1,19 +1,36 @@
 function repo() {
-    repo_ssh_url=$(git config --get remote.origin.url)
-    username=$(echo $repo_ssh_url | awk -F 'git@github.com:|/' '{print $2}')
-    repo_name=$(echo $repo_ssh_url | awk -F '${username}|/' '{print $2}')
-    repo_url="https://github.com/$username/$repo_name"
+    repo_url=$(git config --get remote.origin.url)
+    request_url=""
+
+    if [[ $repo_url == "https://"* ]]; then
+        $request_url=$repo_url
+    else
+        username=$(echo $repo_url | awk -F 'git@github.com:|/' '{print $2}')
+        repo_name=$(echo $repo_url | awk -F '${username}|/' '{print $2}')
+        request_url="https://github.com/$username/$repo_name"
+    fi
+
+    if [[ "$1" == "-b" && -n "$2" ]]; then
+        branch_name=$2
+        request_url="$request_url/tree/$branch_name"
+    fi
+
+    if [[ "$1" == "-u" && -n "$2" ]]; then
+        username=$2
+        request_url="https://github.com/search?q=${username}&type=users";
+    fi
+
     os_type=$(uname)
 
     case "$os_type" in
         "Linux")
-            xdg-open "$repo_url" 2>/dev/null || echo "Error: Cannot open the link."
+            xdg-open "$request_url" 2>/dev/null || echo "Error: Cannot open the link."
             ;;
         "Darwin")
-            open "$repo_url" || echo "Error: Cannot open the link."
+            open "$request_url" || echo "Error: Cannot open the link."
             ;;
         "CYGWIN"*|"MINGW"*)
-            start "$repo_url" || echo "Error: Cannot open the link."
+            start "$request_url" || echo "Error: Cannot open the link."
             ;;
         *)
             echo "Non-supported OS : $os_type"
